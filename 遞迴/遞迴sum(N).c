49adf46993f58771bac620a65fe53fc5a825ec04
zoeyy89 <100426800+zoeyy89@users.noreply.github.com>
2023-08-24 16:42:48 +0800

    Update 遞迴sum(N).c

--- 遞迴/遞迴sum(N).c
+++ 遞迴/遞迴sum(N).c
@@ -8,20 +8,19 @@ int main()
 	return 0;
 }
  
-int sum(int N) {
+int sum(int N)
+{
+	//pass1 sum(3)
+	//pass2 3 + sum(2)
+	//pass3 3 + 2 + sum(1)
+	//pass4 3 + 2 + 1 + F(0)
+	if (N == 0)
 	{
-		//pass1 sum(3)
-		//pass2 3 + sum(2)
-		//pass3 3 + 2 + sum(1)
-		//pass4 3 + 2 + 1 + F(0)
-		if (N == 0)
-		{
-			return 0;
-		}
-		else
-		{
-			return N + sum(N-1);
-		}
+		return 0;
+	}
+	else
+	{
+		return N + sum(N-1);
 	}
 }
  
