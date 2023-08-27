050726812c6a1ba221c0a00a27c6dc363bda06a7
zoeyy89 <100426800+zoeyy89@users.noreply.github.com>
2023-08-27 11:29:31 +0800

    Update palindrome.cpp

--- 練習/palindrome.cpp
+++ 練習/palindrome.cpp
@@ -5,7 +5,7 @@ using namespace std;
 // 5 4 3 2 1 1 2 3 4 5 
 //思考：
 //一個從前向後、一個從後向前
-char data[10] = { 5, 4, 3, 2, 1, 1, 2, 3, 4, 5};
+char data[10] = { 5, 4, 9, 2, 1, 1, 2, 3, 4, 5};
 int n = (sizeof(data) / sizeof(char));
 char* stack;
 
@@ -19,7 +19,7 @@ bool palindrome(char *d, int n)
 	{
         for (int i = 0; i <= (n/2) ; i ++)
 		{
-			if (data[i] != data[n - i - 1]
+			if (data[i] != data[n - i - 1])
 				return false;
 		}
 		return true;
@@ -28,10 +28,10 @@ bool palindrome(char *d, int n)
 
 int main()
 {
-	palindrome(data, n);
-	if (palindrome(data, n) == false)
-		cout << "f" << endl;
-	else if (palindrome(data, n) == true)
-	    cout << "t" << endl;
+	bool isPalindrome = palindrome(data, n);
+	if (isPalindrome)
+		cout << "t" << endl;
+	else
+	    cout << "f" << endl;
     return 0;
 }
