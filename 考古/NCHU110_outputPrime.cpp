861f6c247cf00e119dcaddc13f9939eafb393fe5
zoeyy89 <100426800+zoeyy89@users.noreply.github.com>
2023-11-23 21:46:42 +0800

    Update NCHU110_outputPrime.cpp

--- 考古/NCHU110_outputPrime.cpp
+++ 考古/NCHU110_outputPrime.cpp
@@ -9,29 +9,28 @@ using namespace std;
 
 void primeNumber(int n)
 {
-	if ( n == 1)
-	    cout << "1 不是質因數";
-	else if (n < 1)
-	    cout << "非1正整數才有質因數";
-	else 
-	{
-		for (int i = 2; i <= n; i++)
-	  {
-	    	if (n % i == 0)
-	    	{
-	    	    cout << i << " ";
-	          while(n % i == 0)
-	          {
-			        n = n / i;
-	          }
-	    	}
-    }
-	}
- }
+    if ( n == 1)
+        cout << "1 不是質因數";
+    else if (n < 1)
+	       cout << "非1正整數才有質因數";
+	   else 
+   	{
+        for (int i = 2; i <= n; i++)
+        {
+            if (n % i == 0)
+            {
+                cout << i << " ";
+                while(n % i == 0)
+			                 n = n / i;
+            }
+        }
+   	}
+}
 
 
-int main() {
-	int n = 45;
-	primeNumber(n);
-	return 0;
+int main() 
+{
+    int n = 45;
+    primeNumber(n);
+    return 0;
 }
