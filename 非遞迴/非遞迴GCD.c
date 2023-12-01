1440d7a1e617ea1506addb6a723dbd9d767f529d
zoeyy89 <100426800+zoeyy89@users.noreply.github.com>
2023-12-01 11:37:33 +0800

    Update 非遞迴GCD.c

--- 非遞迴/非遞迴GCD.c
+++ 非遞迴/非遞迴GCD.c
@@ -10,10 +10,10 @@ int GCD(int a, int b)
 
     while ((a != 0)&&(b != 0))
     {
-	if (a > b)
-	    a = a % b;
-	else
-	    b = b % a;
+	    if (a > b)
+	        a = a % b;
+	    else
+	        b = b % a;
     }
     if (a == 0)
 	return b;
